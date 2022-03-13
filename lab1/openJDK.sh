b63e6d24dd55cfec27815d92201b22b33a2b1c54
evpeople <hangcaihui@gmail.com>
2022-03-13 19:00:04 +0800

    add ' in openjdk

--- lab1/openJDK.sh
+++ lab1/openJDK.sh
@@ -3,8 +3,8 @@ cp ~/lab2/OpenJDK8U-jdk_aarch64_linux_openj9_8u292b10_openj9-0.26.0.tar /usr/lib
 cd /usr/lib/jvm/
 tar -vxf OpenJDK8U-jdk_aarch64_linux_openj9_8u292b10_openj9-0.26.0.tar
 echo export JAVA_HOME=/usr/lib/jvm/jdk8u292-b10 >>/etc/profile
-echo export PATH=$JAVA_HOME/bin:$PATH >>/etc/profile
-echo export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar>>/etc/profile
+echo export 'PATH=$JAVA_HOME/bin:$PATH' >>/etc/profile
+echo export 'CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar'>>/etc/profile
 source /etc/profile
 java -version
 
