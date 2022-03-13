603e9f1949e40edfabe9765fedabc129fded5563
evpeople <hangcaihui@gmail.com>
2022-03-13 19:36:29 +0800

    hadoop and test

---
+++ lab1/test.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+accessKey=kkkdsagdfagfdjj
+secretKey=12334345346t4
+endpoint=fsafasdgagffdfgad
+echo "<configuration>
+<property>
+ <name>fs.obs.readahead.inputstream.enabled</name>
+ <value>true</value>
+</property>
+<property>
+ <name>fs.obs.buffer.max.range</name>
+ <value>6291456</value>
+</property>
+<property>
+ <name>fs.obs.buffer.part.size</name>
+ <value>2097152</value>
+</property>
+<property>
+ <name>fs.obs.threads.read.core</name>
+ <value>500</value>
+</property>
+<property>
+ <name>fs.obs.threads.read.max</name>
+ <value>1000</value>
+</property>
+<property>
+ <name>fs.obs.write.buffer.size</name>
+ <value>8192</value>
+</property>
+<property>
+ <name>fs.obs.read.buffer.size</name>
+ <value>8192</value>
+</property>
+<property>
+ <name>fs.obs.connection.maximum</name>
+ <value>1000</value>
+</property>
+<property>
+ <name>fs.defaultFS</name>
+ <value>hdfs://node1:8020</value>
+</property>
+<property>
+ <name>hadoop.tmp.dir</name>
+ <value>/home/modules/hadoop-2.8.3/tmp</value>
+</property>
+<property>
+ <name>fs.obs.access.key</name>
+ <value>$accessKey</value>
+</property>
+<property>
+ <name>fs.obs.secret.key</name>
+ <value>$secretKey</value>
+</property>
+<property>
+ <name>fs.obs.endpoint</name>
+ <value>$endpoint</value>
+</property>
+<property>
+ <name>fs.obs.buffer.dir</name>
+ <value>/home/modules/data/buf</value>
+</property>
+<property>
+ <name>fs.obs.impl</name>
+ <value>org.apache.hadoop.fs.obs.OBSFileSystem</value>
+</property>
+<property>
+ <name>fs.obs.connection.ssl.enabled</name>
+ <value>false</value>
+</property>
+<property>
+ <name>fs.obs.fast.upload</name>
+ <value>true</value>
+</property>
+<property>
+ <name>fs.obs.socket.send.buffer</name>
+ <value>65536</value>
+</property>
+<property>
+ <name>fs.obs.socket.recv.buffer</name>
+ <value>65536</value>
+</property>
+<property>
+ <name>fs.obs.max.total.tasks</name>
+ <value>20</value>
+</property>
+<property>
+ <name>fs.obs.threads.max</name>
+<value>20</value>
+</property>
+</configuration>" > /home/modules/hadoop-2.7.7/etc/hadoop/core-site.xml
