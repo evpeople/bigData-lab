5c66acd3c2bacba747a329c58b155d39579bf285
evpeople <hangcaihui@gmail.com>
2022-03-13 20:44:10 +0800

    debug

--- lab1/updateEnvForHadoop.sh
+++ lab1/updateEnvForHadoop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export HADOOP_HOME=/home/modules/hadoop-2.7.7>>/etc/profile
+echo 'export HADOOP_HOME=/home/modules/hadoop-2.7.7'>>/etc/profile
 echo 'export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH'>>/etc/profile
 export 'HADOOP_CLASSPATH=/home/modules/hadoop2.7.7/share/hadoop/tools/lib/*:$HADOOP_CLASSPATH' >>/etc/profile
 source /etc/profile
